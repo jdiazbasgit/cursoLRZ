63e1ea38f6922a57cf4dbfa1d3d83426d54c0e12
marsanchispa <marsanchispa@hotmail.com>
2023-10-17 17:05:37 +0200

    añadidos comentarios al codigo

--- Nueva carpeta/script.js
+++ Nueva carpeta/script.js
@@ -1,10 +1,10 @@
 
 function ver (){
-    var divContenedor=document.querySelector("#divContenedor") // querySelector recupera el objeto divContenedor, y lo meto en una var
-    var h1Nuevo=document.createElement("h1") //crea un elemento de tipo H1 vacío en el documento, y lo meto en la var h1Nuevo
-    h1Nuevo.innerHTML="Has pulsado el botón" //añado el texto "has pulsado el botón" en divContenedor
-    divContenedor.classList.add("bg-danger") //añade la clase bg-success
-    divContenedor.appendChild(h1Nuevo) // crea el h1 como hijo del div
+    var divContenedor=document.querySelector("#divContenedor") //querySelector recupera el objeto divContenedor > lo meto en la var divContenedor
+    var h1Nuevo=document.createElement("h1") //createElement crea un elemento de tipo H1 vacío en el documento > lo meto en la var h1Nuevo
+    h1Nuevo.innerHTML="Has pulsado el botón" //añado el texto "has pulsado el botón" en la var divContenedor
+    divContenedor.classList.add("bg-danger") //añade la clase bg-success a la var divContenedor
+    divContenedor.appendChild(h1Nuevo) //appendChild crea el h1 como hijo de divContenedor
 
     var boton=document.querySelector("#boton")
     boton.classList.remove("bg-danger")
