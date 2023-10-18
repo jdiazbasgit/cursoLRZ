a346550733c6f7ad7c76a63263bb3ab42f056278
Your Name <you@example.com>
2023-10-18 10:00:18 +0200

    añadido java

---
+++ Nueva carpeta/script.js
@@ -0,0 +1,10 @@
+
+
+function ver (){
+    var divContenedor=document.querySelector("#divContenedor")
+    var h1Nuevo=document.createElement("h1")
+    h1Nuevo.innerHTML="has pulsado el boton"
+    divContenedor.appendChild(h1Nuevo)
+    var boton=document
+ 
+}
