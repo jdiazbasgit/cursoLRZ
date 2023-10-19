b12119f07afcd258b221458926df70ce18c1bb49
Juan Carlos <jcgarcia@dreamroomworld.com>
2023-10-19 13:23:32 +0200

    Juego con variables e if

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function ver(botonParaPulsar) {
     console.log(botonParaPulsar.classList)
-   var divContenedor=document.querySelector("#divContenedor")
+    var divContenedor = document.querySelector("#divContenedor")
     var h1Nuevo = document.createElement("h1")
     h1Nuevo.innerHTML = "has pulsado el boton"
     divContenedor.classList.add("bg-danger")
@@ -10,4 +10,19 @@ function ver(botonParaPulsar) {
     //var boton=document.querySelector("#boton")
     botonParaPulsar.classList.remove("bg-danger")
     botonParaPulsar.classList.add("bg-success")
+
+    var edad = 15;
+    var salario = 50;
+    if (edad <= 15)
+        salario = 20
+    let edad1 = 58;
+    console.log("dentro edad vale:" + edad1);
+// al no tener el ; considera que solo la primera linea es parte del if
+
+    edad++
+    // ++ añade 1 al valor edad
+
+    console.log("el salario vale:" + salario)
+    console.log("fuera la edad vale:" + edad);
+
 }
