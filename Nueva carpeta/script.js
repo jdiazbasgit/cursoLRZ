8edb56cb81a439ca62801396573f08238eb4cf4c
Your Name <you@example.com>
2023-10-19 10:41:19 +0200

    actualizacion

--- Nueva carpeta/script.js
+++ Nueva carpeta/script.js
@@ -1,10 +1,12 @@
 
 
-function ver (){
+function ver (boton){
     var divContenedor=document.querySelector("#divContenedor")
     var h1Nuevo=document.createElement("h1")
     h1Nuevo.innerHTML="has pulsado el boton"
     divContenedor.appendChild(h1Nuevo)
-    var boton=document
+    // var boton=document.querySelector("#boton")
+    boton.classList.remove("bg-danger")
+    boton.classList.add("bg-sucess")
  
 }
