dc3670b6ba90c9cae67175ff20b845a147c3e92f
Ignacio-bc <ignaciobasilecairo@gmail.com>
2023-10-23 13:24:36 +0200

    h

--- g/script1.js
+++ g/script1.js
@@ -1,21 +1,21 @@
 
 
-function ver(botonParaPulsar) {
-    console.log(botonParaPulsar.classList)
-   var divContenedor=document.querySelector("#divContenedor")
-    var h1Nuevo = document.createElement("h1")
-
-i=0
-for(let i=0;i<101;i++){
-    console.log(`valor de i: ${i}`)
-}
+//function ver(botonParaPulsar) {
+//    console.log(botonParaPulsar.classList)
+//   var divContenedor=document.querySelector("#divContenedor")
+//    var h1Nuevo = document.createElement("h1")
+//
+for(let i=0;i<51;i++){
 if(i % 2 !=0){
     console.log(`valor de i: ${i}`)
-    contador++
+    let contenedorpar=document.querySelector("#contendorpar");
+    let h1Nuevo = document.createElement("h1");
+    h1Nuevo.innerHTML = i;
 }
+}
+for(let i=50;i<101;i++){
 if(i % 2 ==0){
     console.log(`valor de i: ${i}`)
-    contador++
 }
-
 }
+//}
