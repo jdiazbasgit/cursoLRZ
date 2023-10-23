f24acf027784b274c5b384b818d445a193066f33
Nacho <jsotosrodrigue@gmail.com>
2023-10-23 11:38:35 +0200

    cambios 2

--- script_nsr.js
+++ script_nsr.js
@@ -11,11 +11,20 @@ function ver(botonParaPulsar) {
     botonParaPulsar.classList.remove("bg-danger")
     botonParaPulsar.classList.add("bg-success")
 
-    var edad = 15
+    /*var edad = 15
     if (edad <= 15) {
         let edad = 58
         console.log("dentro edad vale;" + edad)
     }
     console.log("fuera edad vale;" + edad)
-    //comilla simple control + ?
+    //comilla simple control + ?*/
 }
+
+
+//for(valor de inicio; condicion que tengo  que cumplir;incremento o decremento)
+
+for(let i=0;i<10;i++){
+    console.log(`valor de i: ${i}`)
+
+}
+
