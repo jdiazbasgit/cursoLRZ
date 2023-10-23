2114727ef8fff114569f9bdaecf4532d65fc5837
Juan Carlos <jcgarcia@dreamroomworld.com>
2023-10-23 17:23:35 +0200

    Ejercicio de numeros pares e impares

--- script_JCGA.js
+++ script_JCGA.js
@@ -1,6 +1,7 @@
 
 
-function ver(botonParaPulsar) {
+//function ver(botonParaPulsar) {
+var ver = (botonParaPulsar) => {
     console.log(botonParaPulsar.classList)
     var divContenedor = document.querySelector("#divContenedor")
     var h1Nuevo = document.createElement("h1")
@@ -11,3 +12,70 @@ function ver(botonParaPulsar) {
     botonParaPulsar.classList.remove("bg-danger")
     botonParaPulsar.classList.add("bg-success")
 }
+
+var edad = 14
+var salario = 50
+if (edad <= 15) {
+    salario = 20
+    /*let edad=58
+    console.log("dentro edad vale:"+edad)*/
+}
+console.log("slaario=" + salario)
+edad++
+console.log("fuera edad vale:" + edad)
+
+//for(valor de inicio;condicion;incremento o decremento)
+i = 0
+for (let i = 0; i < 10; i++) {
+    nombre = "mi nombre es\"javi\""
+    n = `mi nombre es "javier"`
+    console.log(`valor de i: ${i}`)
+}
+
+if (i % 2 != 0) {
+    console.log(`valor de i: ${i}`)
+    contador++
+}
+
+//arrays 
+
+let numeros = new Array()
+let numeros1 = []
+if (true) {
+    let numeros = 3
+}
+let numeros2 = [3, 6, 5, 7, 9, 34]
+numeros2[5] = 15
+console.log(numeros)
+console.log(numeros1)
+console.log(numeros2)
+for (i = 0; i < numeros.length; i++)
+    console.log(`valor del array: ${numeros2[i]}`)
+numeros2.forEach(function (elemento) {
+    console.log(`elemento dentro del foreach es: $(elemento+100)`)
+})
+
+let verNumeros = (fin) => {
+    let divEjercicio1 = document.querySelector("#numeros")
+
+    for (let i = 0; i < fin; i++) {
+        if (i <= 50 && i % 2 == 0) {
+            // console.log(`los pares menores de 50 son: ${i}`). En lugar de por consola lo voy a hacer en el html
+            let h1Nuevo = document.createElement("h1")
+            h1Nuevo.innerHTML = i
+            if ((i <= fin / 2) && (i % 2 == 0)) {
+                h1Nuevo.classList.add("bg-danger", "text-center")
+                divEjercicio1.appendChild(h1Nuevo)
+            }
+            if ((i > fin / 2) && (i % 2 != 0)) {
+                h1Nuevo.classList.add("bg-success", "text-center")
+                divEjercicio1.appendChild(h1Nuevo)
+            }
+        }
+
+    }
+
+}
+
+
+
