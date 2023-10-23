48a007d80351490dcf50b362b97c231612c14fc3
Ignacio-bc <ignaciobasilecairo@gmail.com>
2023-10-23 11:38:13 +0200

    a

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ function ver(botonParaPulsar) {
     botonParaPulsar.classList.remove("bg-danger")
     botonParaPulsar.classList.add("bg-success")
 }
+
+var edad=14 
+var salario=50
+if(edad<=15)
+{
+    salario=20
+    /*let edad=58
+    console.log("dentro edad vale:"+edad)*/
+}
+console.log("slaario="+salario)
+edad++
+console.log("fuera edad vale:"+edad)
+
+//for(valor de inicio;condicion;incremento o decremento)
+i=0
+for(let i=0;i<10;i++){
+    nombre="mi nombre es\"javi\""
+    n=`mi nombre es "javier"`
+        console.log(`valor de i: ${i}`)
+}
+
+if(i % 2 !=0){
+    console.log(`valor de i: ${i}`)
+    contador++
+}
+
+//arrays 
+
+let numeros=new Array()
+let numeros1=[]
+if(true){
+    let numeros=3 
+}
+let numeros2=[3,6,5,7,9,34]
+numeros2[5]=15
+console.log(numeros)
+console.log(numeros1)
+console.log(numeros2)
+for(i=0;i<numeros.length;i++)
+console.log(`valor del array: ${numeros2[i]}`)
+numeros2.forEach(function(elemento){
+    console.log(`elemento dentro del foreach es: $(elemento+100)`)
+})
+
+
+//arrow functioni
