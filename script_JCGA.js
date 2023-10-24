6c7785d2d026df741cf78db22a195e898b876f7b
Juan Carlos <jcgarcia@dreamroomworld.com>
2023-10-24 13:28:50 +0200

    Prueba entre errores

--- script_JCGA.js
+++ script_JCGA.js
@@ -141,7 +141,7 @@ let verNumeros2 = () => {
 let suma = (...numeros) => {
     // los ... hace que lo que viene detras sea un array, autodefinido, sin necesidad de declarar la variable
     let salida = 0
-    
+
     numeros.forEach((numero) => {
         // el forEach se usa cuando tenemos colecciones de datos
         salida += numero
@@ -154,7 +154,51 @@ console.log(suma(3, 4, 56, 2, 74))
 console.log(suma(3, 4, 56, 4, 67, 34, 23, 12, 3))
 
 // comando indexOf. Me devuelve la posicion en la que se encuentra lo buscado
-let texto="123456789"
-console.log("position:"+texto.indexOf(4))
-console.log("position:"+texto.indexOf(0))
+let texto = "123456789"
+console.log("position:" + texto.indexOf(4))
+console.log("position:" + texto.indexOf(0))
 // me devuelve -1 si no existe en la cadena
+
+// Vamos con el While..., mientras se cumpla la condición , ejecuto el codigo    while(condicion){}
+// Si quiero que siempre se ejecute algo, al menos una vez, se usa el do while...., con la sintaxis   do{} while()
+
+// Ejercicio de ordenamiento por el metodo de la burbuja 
+
+let numerosPrimitiva = [5, 4, 9, 3, 7, 2, 1];
+let termine = false;
+// while termine==false; es igual que la siguiente pero menos eficiente
+console.log("antes de ordenar:" + numerosPrimitiva)
+while (!termine) {
+    termine = true
+    for (let i = 0; i < numerosPrimitiva.length - 1; i++) {
+        if (numerosPrimitiva[i] > numerosPrimitiva[i + 1]) {
+            termine = false
+            let temporal = numerosPrimitiva[i + 1]
+            // meto el segundo en un temporal para luego cambiarlo
+            numerosPrimitiva[i + 1] = numerosPrimitiva[i]
+            numerosPrimitiva[i] = temporal
+        }
+    }
+}
+console.log("despues de ordenar:" + numerosPrimitiva)
+
+// otro de lo mismo....Calcular aleatoriamente los numeros de la primitiva (1 a 49) y ordénalo. Necesito "indexOf" (me dice si está o no en el array), para que no se repita; necesito incluir en un array, "push"; necesito mirar la longitud del array y que sea menor que 7; necesito generar un random (que es entre 0 y 1); necesito redondear el nuemro aleatorio (tofixed(0))
+
+let numerosNuevosPrimitiva = []
+let numeroNuevo = () => {
+    while (numerosNuevosPrimitiva.length < 7) {
+        let nuevoNumero = Math.random().toFixed(0) * 100;
+        if (nuevoNumero > 49) {
+            nuevoNumero = Math.random().toFixed(0) * 100
+        }
+        if (numerosNuevosPrimitiva.indexOf(nuevoNumero) = !-1) {
+            numerosNuevosPrimitiva.push(nuevoNumero)
+        }
+
+    }
+
+}
+
+console.log("los numeros ganadores son:" + numerosNuevosPrimitiva)
+
+
