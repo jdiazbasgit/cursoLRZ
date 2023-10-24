b1afa4d4bbb2d6b4061202a34acc5b4d2ddfd89e
Juan Carlos <jcgarcia@dreamroomworld.com>
2023-10-24 10:24:56 +0200

    Juego con numeros pares e impares

--- script_JCGA.js
+++ script_JCGA.js
@@ -54,6 +54,33 @@ for (i = 0; i < numeros.length; i++)
 numeros2.forEach(function (elemento) {
     console.log(`elemento dentro del foreach es: $(elemento+100)`)
 })
+// dos formas... con un nuemro fijo de 100 y en segundo lugar con un parametro pasado desde HTML
+
+function verNumeros1(){
+    // creas una variable con el valor final de la serie
+        let divEjercicio1 = document.querySelector("#ejercicio1")
+    // cojes lo que tengas en ejercicio1 y lo metes en la variable divEjercicio1
+        for (let i = 0; i < 100; i++) {
+            let h1Nuevo = document.createElement("h1")
+            // crea una variable h1Nuevo cuyo contenido va a ser un h1
+            h1Nuevo.innerHTML = i
+            // rellena ese h1 con el valor de i
+    
+            if ((i <= 50) && (i % 2 == 0)) {
+                    // si i es menor que la mitad de "fin" y es par
+                    h1Nuevo.classList.add("bg-danger", "text-center")
+                    // da a h1Nuevo esas caracteristicas bg color y centrado
+                    divEjercicio1.appendChild(h1Nuevo)
+                    // escribe el h1Nuevo dentro del contenedor divEjercicio1
+                }
+            if ((i > 50) && (i % 2 != 0)) {
+                // para todos los impares superiores a la mitad del limite
+                    h1Nuevo.classList.add("bg-success", "text-center")
+                    divEjercicio1.appendChild(h1Nuevo)
+                }
+            }
+    
+        }
 
 let verNumeros = (fin) => {
 // creas una variable con el valor final de la serie
@@ -81,5 +108,33 @@ let verNumeros = (fin) => {
 
     }
 
+    let verNumeros2 = () => {
+        let final = document.querySelector("#cantidad").value
+        // creas una variable con el valor final de la serie
+            let divEjercicio1 = document.querySelector("#ejercicio1")
+        // cojes lo que tengas en ejercicio1 y lo metes en la variable divEjercicio1
+            for (let i = 0; i < final; i++) {
+                let h1Nuevo = document.createElement("h1")
+                // crea una variable h1Nuevo cuyo contenido va a ser un h1
+                h1Nuevo.innerHTML = i
+                // rellena ese h1 con el valor de i
+        
+                if ((i <= final / 2) && (i % 2 == 0)) {
+                        // si i es menor que la mitad de "fin" y es par
+                        h1Nuevo.classList.add("bg-danger", "text-center")
+                        // da a h1Nuevo esas caracteristicas bg color y centrado
+                        divEjercicio1.appendChild(h1Nuevo)
+                        // escribe el h1Nuevo dentro del contenedor divEjercicio1
+                    }
+                if ((i > final / 2) && (i % 2 != 0)) {
+                    // para todos los impares superiores a la mitad del limite
+                        h1Nuevo.classList.add("bg-success", "text-center")
+                        divEjercicio1.appendChild(h1Nuevo)
+                    }
+                }
+        
+            }
+
+// vamos a ver el operador spread que se escribe con ...  tres puntitos
 
 
