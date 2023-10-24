e85444a4d5e23eb755599cc9c06c6ee6580d1692
ManuelNoriega <mnoriegacosta@gmail.com>
2023-10-24 11:06:57 +0200

    capa

--- for.js
+++ for.js
@@ -1,30 +1,32 @@
-function ver(botonParaPulsar) {
-   console.log(botonParaPulsar.classList)
-  var divContenedor=document.querySelector("#divContenedor")
-   var h1Nuevo = document.createElement("h1")
-   h1Nuevo.innerHTML = "has pulsado el boton"
-   divContenedor.classList.add("bg-danger")
-   divContenedor.appendChild(h1Nuevo)
-   //var boton=document.querySelector("#boton")
-   botonParaPulsar.classList.remove("bg-danger")
-   botonParaPulsar.classList.add("bg-success")
-}
 
- for(let i=0; i<=100; i++){  
-   if (i % 2 == 0 ) {
-      console.log(`valor de i en pares: ${i}`)   
+
+
+let verNumeros=(fin)=>{
+   let ejercicio1 = document.querySelector("#ejercicio1")
+
+   for(let i = 0; i< fin; i++){ 
+
+   if (i <=fin/2 && i % 2 == 0 ) {
+     
+      let h1 = document.createElement("h1")
+      h1.innerHTML = i
+      h1.classList.add ("text-center")
+      h1.classList.add ("bg-danger")
+      ejercicio1.appendChild(h1)
+      // console.log(`valor de i en pares: ${i}`)   
+   } 
+   if (i > fin/2 && i % 2 != 0 ) {
+     
+      let h1 = document.createElement("h1")
+      h1.classList.add  ("text-center")   
+      h1.innerHTML = i
+      h1.classList.add  ("bg-success")
+      ejercicio1.appendChild(h1)
+      // console.log(`valor de i en pares: ${i}`)   
    } 
 }
-   
+}  
 
-  
   
       
     
-
-// for(let i=0; i<=51; i+=2){    
-//    console.log(`valor de i: ${i}`)
-// }
-// for(let i=53; i<=100; i+=3){    
-//    console.log(`valor de i: ${i}`)
-// }
