70a466f4df0fe39fc16579f0629dd5b1b82edab1
marsanchispa <marsanchispa@hotmail.com>
2023-10-26 12:21:41 +0200

    creados documentos primitiva

--- script_Mar.js
+++ script_Mar.js
@@ -66,54 +66,82 @@
 
 //array con los 6 numeros de la primitiva:
 
-let numerosPrimi = new Array();
+// let numerosPrimi = new Array();
 
-function dameNumero() { //dameNumero = ()=>{
-    let numero = 0;
-    while (numero == 0 || numero > 49) {
-        valido = true;
-        numero = Math.trunc(Math.random() * 100); //eq: Math.floor(Math.random()*49 +1);
+// function dameNumero() { //dameNumero = ()=>{
+//     let numero = 0;
+//     while (numero == 0 || numero > 49) {
+//         valido = true;
+//         numero = Math.trunc(Math.random() * 100); //eq: Math.floor(Math.random()*49 +1);
+//     }
+//     return numero;
+// }
+
+// let max=6;
+// while (numerosPrimi.length < max) {
+//     let num = dameNumero();
+//     if (!numerosPrimi.includes(num)) // equivalente: if (numerosPrimi.indexOf(num) === -1)
+//         numerosPrimi.push(num); 
+// }
+
+// // nombreArray.sort(a,b) SORT ordena un array
+// let termine = false;
+// while (!termine) { // while(termine==false)
+//     termine = true;
+//     for (let i = 0; i < numerosPrimi.length - 1; i++) {
+//         if (numerosPrimi[i] > numerosPrimi[i + 1]) {
+//             termine = false;
+//             let temporal = numerosPrimi[i + 1];
+//             numerosPrimi[i + 1] = numerosPrimi[i];
+//             numerosPrimi[i] = temporal;
+//         }
+//     }
+// }
+
+// function muestra(arrayNumeros) {
+
+//     let contenedorpares = document.querySelector("#contenedorpares");
+//     let h1Nuevo = document.createElement("h1");
+//     h1Nuevo.innerHTML = "Tus números de la primitiva son:";
+//     contenedorpares.classList.add("text-center");
+//     contenedorpares.appendChild(h1Nuevo);
+
+//     for (let i = 0; i < arrayNumeros.length; i++) {
+//         let contenedorimpares = document.querySelector("#contenedorimpares");
+//         let pNuevo = document.createElement("p");
+//         pNuevo.innerHTML = arrayNumeros[i];
+//         pNuevo.classList.add("text-center");
+//         contenedorimpares.appendChild(pNuevo);
+//     }
+//     console.log(`Tus numeros para la Primitiva son: ${arrayNumeros}`);
+// }
+
+// muestra(numerosPrimi);
+
+ejemploSwitch = numero => { // ejemploSwitch=(numero)=>{
+    let div = document.querySelector("#contenido");
+    let h1 = document.createElement("h1");
+    switch (numero) {
+        case 1:
+            h1.innerHTML="Has pulsado el botón 1";
+            break;
+        case 2:
+            h1.innerHTML="Has pulsado el botón 2";
+            break;
+        case 3:
+            h1.innerHTML="Has pulsado el botón 3";
+            break;
+        case 4:
+            h1.innerHTML="Has pulsado el botón 4";
+            break;
+        case 5:
+            h1.innerHTML="Has pulsado el botón 5";
+            break;
+        default:
+            h1.innerHTML="No has pulsado ninguno de los botones";
+
     }
-    return numero;
+    div.innerHTML = "";
+    div.appendChild(h1);
+
 }
-
-let max=6;
-while (numerosPrimi.length < max) {
-    let num = dameNumero();
-    if (!numerosPrimi.includes(num)) // equivalente: if (numerosPrimi.indexOf(num) === -1)
-        numerosPrimi.push(num);
-    
-}
-
-let termine = false;
-while (!termine) { // while(termine==false)
-    termine = true;
-    for (let i = 0; i < numerosPrimi.length - 1; i++) {
-        if (numerosPrimi[i] > numerosPrimi[i + 1]) {
-            termine = false;
-            let temporal = numerosPrimi[i + 1];
-            numerosPrimi[i + 1] = numerosPrimi[i];
-            numerosPrimi[i] = temporal;
-        }
-    }
-}
-
-function muestra(arrayNumeros) {
-
-    let contenedorpares = document.querySelector("#contenedorpares");
-    let h1Nuevo = document.createElement("h1");
-    h1Nuevo.innerHTML = "Tus números de la primitiva son:";
-    contenedorpares.classList.add("text-center");
-    contenedorpares.appendChild(h1Nuevo);
-
-    for (let i = 0; i < arrayNumeros.length; i++) {
-        let contenedorimpares = document.querySelector("#contenedorimpares");
-        let pNuevo = document.createElement("p");
-        pNuevo.innerHTML = arrayNumeros[i];
-        pNuevo.classList.add("text-center");
-        contenedorimpares.appendChild(pNuevo);
-    }
-    console.log(`Tus numeros para la Primitiva son: ${arrayNumeros}`);
-}
-
-muestra(numerosPrimi);
