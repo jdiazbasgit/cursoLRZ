478cbc6a5d369867ad3bc3b5459bf6de19cc4784
Javier <javier7moreno@hotmail.com>
2023-10-30 10:46:58 +0100

    nuevos actualizaciones de archivos de Objeto

--- objetos/Animal_Javi.js
+++ objetos/Animal_Javi.js
@@ -1,8 +1,11 @@
 //Crea una clase Animal con las siguientes propiedades: raza y especie
-export class Animal{
-    
-    raza=""
-    especie=""
-    
-    constructor
+export class Animal { //El export es para que me vean desde afuera, si no lo pongo tendría que tener todo el codigo en este archivo
+
+    raza = ""
+    especie = ""
+
+    constructor(raza, especie) { //Se declara el valor de la propiedad
+        this.raza = raza
+        this.especie = especie
+    }
 }
--- objetos/verAnimales_Javi.js
+++ objetos/verAnimales_Javi.js
@@ -1,10 +1,12 @@
-document.querySelector("#boton").addEventListener("click",function(){
-    let perro = new Animal ("caniche","canina")
-    let div=document.querySelector("#contenido")
-    let h1raza=document.createElement("h1")
-    let h1especie=document.createElement("h1")
-    h1Raza.innerHTML=`La raza es ${perro.raza}`
-    h1Especie.innerHTML=`La especie es ${perro.especie}`
-    div.appendChild(h1raza)
+import { Animal } from "./clases/Animal_Javi.js"
+
+document.querySelector("#boton").addEventListener("click", function () {
+    let perro = new Animal("caniche", "canina")
+    let div = document.querySelector("#contenido")
+    let h1Raza = document.createElement("h1")
+    let h1Especie = document.createElement("h1")
+    h1Raza.innerHTML = `La raza es ${perro.raza}`
+    h1Especie.innerHTML = `La especie es ${perro.especie}`
+    div.appendChild(h1Raza)
     div.appendChild(h1Especie)
 })
