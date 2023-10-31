91a4b4c2cffa36648a353c3b3423b6b49fa56703
jdiazbasgit <git@studiotapal.es>
2023-10-31 14:27:49 +0100

    primer dia

---
+++ script.js
@@ -0,0 +1,40 @@
+edad = 28
+const PI = 3.1416
+
+/*
+Tipos de datos
+texto
+numero
+booleano(logico)
+Array
+Objeto
+undefined y null
+
+Operadores
+aritmeticos
+*+-/ % ^ **
+asignacion
+=
+operadores lógicos
+
+> < >= <= != !== == ===
+ */
+
+a = 5
+b = "5"
+console.log(a == b)
+console.log(a === b)
+
+texto = "a es menor o igual a 4"
+if (a > 4)
+    texto = "a es mayos que 4"
+else
+    console.log(texto);
+
+texto = "a es menor o igual a 4"
+if (a > 4)
+    texto = "a es mayos que 4"
+console.log(texto);
+
+
+
