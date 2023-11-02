5567fce0fb62d7912a0ae0150b9d0809f2c2b50b
Ignacio-bc <ignaciobasilecairo@gmail.com>
2023-11-02 13:15:03 +0100

    j

--- Ejercicio POO/scriptpoo2.js
+++ Ejercicio POO/scriptpoo2.js
@@ -1,1 +1,32 @@
-a
+
+
+export class Musico {
+
+    instrumentos = new Array()
+    //instrumentos=[]
+    tipo
+
+
+
+    /*constructor(tipo, instrumentos) {
+        this.instrumentos = instrumentos
+        this.tipo = tipo
+    }*/
+
+    constructor(tipo, ...instrumentos) {
+        this.instrumentos = instrumentos
+        this.tipo = tipo
+    }
+
+
+
+    tocar = () => {
+        console.log(`el musico es de tipo ${this.tipo}`)
+
+        this.instrumentos.forEach(instrumento => {
+            console.log(instrumento.sonar())
+        })
+
+    }
+
+}
