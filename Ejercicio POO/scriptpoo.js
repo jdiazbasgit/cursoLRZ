02a48c113bf1f2b8bcfdf8bd0bf3120e9af690f8
Ignacio-bc <ignaciobasilecairo@gmail.com>
2023-11-02 13:10:35 +0100

    a

--- Ejercicio POO/scriptpoo.js
+++ Ejercicio POO/scriptpoo.js
@@ -3,20 +3,20 @@ import { Musico } from "../clases/Musico.js";
 
 
 
-document.querySelector("#boton").addEventListener("click",function(){
-    let tambor=new Instrumento("pom, pom, pom")
-    let instrumentos=new Array()
+document.querySelector("#boton").addEventListener("click", function () {
+    let tambor = new Instrumento("pom, pom, pom")
+    let instrumentos = new Array()
     //let instrumentos=[tambor]
     //instrumentos.push(tambor)
-    instrumentos[0]=tambor
-    let solista=new Musico(instrumentos,"percusion")
+    instrumentos[0] = tambor
+    let solista = new Musico(instrumentos, "percusion")
     solista.tocar()
 })
 
-document.querySelector("#hombreOrquesta").addEventListener("click",function(){
-    let tambor=new Instrumento("pom,pom,pom")
-    let trompeta=new Instrumento("tuuu, tuuuu, tuuuu")
-    let guitarra=new Instrumento("tlan, tlan, tlan")
-    let hombreOrquesta=new Musico([tambor,trompeta,guitarra],"varios")
+document.querySelector("#hombreOrquesta").addEventListener("click", function () {
+    let tambor = new Instrumento("pom,pom,pom")
+    let trompeta = new Instrumento("tuuu, tuuuu, tuuuu")
+    let guitarra = new Instrumento("tlan, tlan, tlan")
+    let hombreOrquesta = new Musico([tambor, trompeta, guitarra], "varios")
     hombreOrquesta.tocar()
 })
