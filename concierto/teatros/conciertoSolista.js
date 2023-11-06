a653ef544f33a38d3225ecfbbfc79b2f914c3c36
marsanchispa <marsanchispa@hotmail.com>
2023-11-06 10:23:05 +0100

    mejorado html

--- concierto/teatros/conciertoSolista.js
+++ concierto/teatros/conciertoSolista.js
@@ -9,6 +9,9 @@ let trompeta = new Instrumento("tuuuu, tuuuu, tuuuu")
 //let solista = new Musico( "percusion",instrumentos)
 let solista = new Musico("percusión", tambor)
 
+let limpiaElemento = elementoAlimpiar => {
+    elementoAlimpiar.innerHTML="";
+}
 
 // let conciertoSolista = () => {
 //     switch (solista.instrumentos.length) {
@@ -23,24 +26,22 @@ let solista = new Musico("percusión", tambor)
 //     }
 // }
 
+let contenido1 = document.querySelector("#contenido1")
 //document.querySelector("#botonSolista").addEventListener("click",conciertoSolista())
 document.querySelector("#botonSolista").addEventListener("click", () => {
-
+    
+    limpiaElemento(contenido1);
     switch (solista.instrumentos.length) {
         case 1:
             //solista.tocar()
-            let contenido1 = document.querySelector("#contenido1")
+            
             let h1Tipo = document.createElement("h1");
             h1Tipo.innerHTML = `Eres un músico de tipo "${solista.tipo}"`;
             contenido1.appendChild(h1Tipo);
 
             let h1Sonido = document.createElement("h1");
-
-            solista.instrumentos.forEach(instrumento => {
-                h1Sonido.innerHTML = `Tu sonido es "${instrumento.sonido}"`;
-                contenido1.appendChild(h1Sonido);
-            })
-
+            h1Sonido.innerHTML = `Tu sonido es "${solista.instrumentos[0].sonar()}"`;
+            contenido1.appendChild(h1Sonido);
             break;
         case 0:
             console.log("no tienes nada que tocar")
@@ -48,12 +49,12 @@ document.querySelector("#botonSolista").addEventListener("click", () => {
         default:
             console.log("tu no eres un hombre orquesta")
     }
-
 })
 
 let hombreOrquesta = new Musico("polivalente", tambor, trompeta)
 
 let conciertoHombreOrquesta = () => {
+    limpiaElemento(contenido1);
     switch (hombreOrquesta.instrumentos.length) {
         case 1:
             console.log("tu no eres un solista")
@@ -61,17 +62,16 @@ let conciertoHombreOrquesta = () => {
         case 0:
             console.log("no tienes nada que tocar")
             break;
-        default:
-            let contenido2 = document.querySelector("#contenido2")
+        default:         
             let h1Tipo = document.createElement("h1");
             h1Tipo.innerHTML = `Eres un músico de tipo "${hombreOrquesta.tipo}"`;
-            contenido2.appendChild(h1Tipo);
+            contenido1.appendChild(h1Tipo);
 
             hombreOrquesta.instrumentos.forEach(instrumento => {
                 console.log(instrumento);
                 let h1Sonido = document.createElement("h1");
                 h1Sonido.innerHTML = `Tu sonido es "${instrumento.sonido}"`;
-                contenido2.appendChild(h1Sonido);
+                contenido1.appendChild(h1Sonido);
             })
     }
 }
