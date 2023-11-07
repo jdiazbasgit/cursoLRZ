aaf1774f2dfd1db393bb3ae5d9262fd650de03af
lujandroid <lujandroid@gmail.com>
2023-11-07 09:35:04 +0100

    cambios

--- objectosjpt/teatro/conciertoSolista_jpt.js
+++ objectosjpt/teatro/conciertoSolista_jpt.js
@@ -3,20 +3,20 @@ import { Musico } from "../clases/Musico_jpt.js";
 
 
 
-document.querySelector("#boton").addEventListener("click",function(){
-    let tambor=new Instrumento("pom, pom, pom")
-    let instrumentos=new Array()
+document.querySelector("#boton").addEventListener("click", function () {
+    let tambor = new Instrumento("pom, pom, pom")
+    let instrumentos = new Array()
     //let instrumentos=[tambor]
     instrumentos.push(tambor)
     //instrumentos[0]=tambor
-    let solista=new Musico(instrumentos,"percusion")
+    let solista = new Musico(instrumentos, "percusion")
     solista.tocar()
 })
 
-document.querySelector("#hombreOrquesta").addEventListener("click",function(){
-    let tambor=new Instrumento("pom,pom,pom")
-    let trompeta=new Instrumento("tuuu, tuuu , tuuu")
-    let guitarra=new Instrumento("tlan, tlan, tlan")
-    let hombreOrquesta=new Musico([tambor,trompeta,guitarra],"varios")
+document.querySelector("#hombreOrquesta").addEventListener("click", function () {
+    let tambor = new Instrumento("pom,pom,pom")
+    let trompeta = new Instrumento("tuuu, tuuu , tuuu")
+    let guitarra = new Instrumento("tlan, tlan, tlan")
+    let hombreOrquesta = new Musico([tambor, trompeta, guitarra], "varios")
     hombreOrquesta.tocar()
 })
