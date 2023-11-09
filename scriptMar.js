1be9ea867a39eb63f6186fecfbd779d49e23e353
marsanchispa <marsanchispa@hotmail.com>
2023-11-09 15:14:45 +0100

    con inputs de texto

--- scriptMar.js
+++ scriptMar.js
@@ -1,61 +1,6 @@
-edad = 28
-const PI = 3.1416
-
-/*
-Tipos de datos
-texto
-numero
-booleano(logico)
-Array
-Objeto
-undefined y null
-
-Operadores
-aritmeticos
-*+-/ % ^ **
-asignacion
-=
-operadores lógicos
-
-> < >= <= != !== == ===
- */
-
-a = 5
-b = "5"
-console.log(a == b)
-console.log(a === b)
-
-texto = "a es menor o igual a 4"
-if (a > 4)
-    texto = "a es mayos que 4"
-else
-    console.log(texto);
-
-texto = "a es menor o igual a 4"
-if (a > 4)
-    texto = "a es mayos que 4"
-console.log(texto);
-
-for (let i = 0; i < 10; i++) {
-    if (i % 3 === 0) {
-        console.log(i);
-    }
-
-}
-let numeros = [4, 7, 45, -8, 5.56, 5, 345, 90]
-let numero = 0
-posicion = 0
-while (numero < 200 || posicion === numeros.length) {
-    console.log(numeros[posicion])
-    posicion++
-    numero = numeros[posicion]
-
-
-}
-
-function suma(a, b) {
-    return a + b;
-}
+// function suma(a, b) {
+//     return a + b;
+// }
 
 //let suma=(a,b)=>a+b
 
@@ -76,23 +21,24 @@ trCabecera.appendChild(th3)
 tabla.appendChild(trCabecera)
 divContenido.appendChild(tabla)
 
+
 let agregarFila = () => {
     let tr = document.createElement("tr")
     let td1 = document.createElement("td")
     let td2 = document.createElement("td")
     let td3 = document.createElement("td")
-    td1.innerHTML="pepe"
-    td2.innerHTML="garcia"
-    td3.innerHTML="8765435f"
+    td1.innerHTML=document.getElementById("nombre").value;
+    td2.innerHTML=document.getElementById("apellido").value;
+    td3.innerHTML=document.querySelector("#dni").value; //2 formas distintas
     tr.appendChild(td1)
     tr.appendChild(td2)
     tr.appendChild(td3)
     tabla.appendChild(tr)
 }
 
-let divJson= document.querySelector("#json")
-divJson.innerHTML=JSON.stringify(document)
-console.log(document.location.port)
+// let divJson= document.querySelector("#json")
+// divJson.innerHTML=JSON.stringify(document)
+// console.log(document.location.port)
 
 
 
