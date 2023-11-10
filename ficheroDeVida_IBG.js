daa5dc154472e57d9bd7bda647d6733198e8e779
Your Name <you@example.com>
2023-11-10 10:17:51 +0100

    cambio de nombre para mi rama

--- ficheroDeVida_IBG.js
+++ ficheroDeVida_IBG.js
@@ -1,8 +1,8 @@
-import { DatosLaborales } from "./clases/DatosLaborales.js";
-import { DatosPersonales } from "./clases/DatosPersonales.js";
-import { Empleado } from "./clases/Empleado.js";
-import { Empresa } from "./clases/Empresa.js";
-import { Hijos } from "./clases/Hijos.js";
+import { DatosLaborales } from "./clases/DatosLaborales_IBG.js";
+import { DatosPersonales } from "./clases/DatosPersonales_IBG.js";
+import { Empleado } from "./clases/Empleado_IBG.js";
+import { Empresa } from "./clases/Empresa_IBG.js";
+import { Hijos } from "./clases/Hijos_IBG.js";
 
 let hijos=new Hijos(3,5)
 let datosLaborales=new DatosLaborales(5000,"jefe de proyecto")
