7b4cfe8e8e5e48a9252f900e71c8fd6ef50ae864
Javier <javier7moreno@hotmail.com>
2023-11-13 11:12:46 +0100

    Duplicando los archivos originales con mi nombre

---
+++ interfaces/InstrumentoInterface_Jm.ts
@@ -0,0 +1,3 @@
+export interface InstrumentoInterface_Jm{
+     sonar():string;
+}
---
+++ interfaces/MusicoInterface_Jm.ts
@@ -0,0 +1,4 @@
+export interface MusicoInterface_Jm{
+     // Se coloca void porque no devolvera nada, cuando queramos que devuelva algo, se le colocaría string y tendria un return
+     sonar():void;
+}
