77f1a5f337112b7b30ded1a22bb9b3c5adfb36aa
Javier <javier7moreno@hotmail.com>
2023-11-14 13:36:38 +0100

    Actualizando el ejercicio inicial

---
+++ ficheroDeVida_Jm1.ts
@@ -0,0 +1,13 @@
+import { Paises } from "./clases/Paises_Jm1.js";
+
+let paises = new Paises()
+let paisesRecibidos: Array<any> = []
+paises.getDatos("https://restcountries.com/v3.1/region/europe").then(datos => {
+    paisesRecibidos = datos
+    let div = document.querySelector("#contenido")
+    paisesRecibidos.forEach(pais => {
+        let h1 = document.createElement("h1")
+        h1.innerHTML = `${pais.name.official} - ${pais.capital}`
+        div?.appendChild(h1)
+    })
+})
