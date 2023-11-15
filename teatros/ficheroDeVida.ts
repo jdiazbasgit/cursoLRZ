af5017c6779f65c8c508c061ac6a22312f333add
Irene <irenebidongomez@gmail.com>
2023-11-15 12:06:29 +0100

    a

--- teatros/ficheroDeVida.ts
+++ teatros/ficheroDeVida.ts
@@ -3,15 +3,16 @@ import { Instrumento } from "../clases/Instrumento.js";
 import { Solista } from "../clases/Solista.js";
 import { Tambor } from "../clases/Tambor.js";
 import { Trompeta } from "../clases/Trompeta.js";
+import { InstrumentoInterface } from "../interfaces/InstrumentoInterface.js";
 
-var tambor:Tambor= new Tambor("pom, pom, pom","tambor")
-var trompeta:Trompeta= new Trompeta("tuuu, tuuu, tuuu","trompeta")
-var instrumentosSolista:Array<Instrumento>=[tambor]
-var instrumentosHombreOrquesta:Array<Instrumento>=[tambor,trompeta]
+var tambor:InstrumentoInterface= new Tambor("pom, pom, pom","tambor")
+var trompeta:InstrumentoInterface= new Trompeta("tuuu, tuuu, tuuu","trompeta")
+var instrumentosSolista:Array<InstrumentoInterface>=[tambor]
+var instrumentosHombreOrquesta:Array<InstrumentoInterface>=[tambor,trompeta]
 var solista:Solista= new Solista(instrumentosSolista,"percusion")
 var hombreOrquesta:HombreOrquesta= new HombreOrquesta(instrumentosHombreOrquesta,"polivalente")
 console.log("va a tocar el solista")
 solista.tocar();
 console.log("va a tocar el hombre orquesta")
 hombreOrquesta.tocar()
-trompeta.dondeEstoy()
+//trompeta.dondeEstoy()
