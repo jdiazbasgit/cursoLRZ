b6b768fbd895343050a3661571d611e166eaf930
jdiazbasgit <git@studiotapal.es>
2023-11-15 11:11:23 +0100

    up

--- ficheroDeVida.ts
+++ ficheroDeVida.ts
@@ -17,22 +17,53 @@ let div = document.querySelector("#contenido")
     div?.appendChild(h1)
 })*/
 
-document.querySelector("#continente")?.addEventListener("change",()=>{
-    let selectContinentes=document.querySelector("#continente")
-   let selectPaises=document.querySelector("#paises")
-    paises.getDatos("https://restcountries.com/v3.1/region/"+
-     (selectContinentes as HTMLSelectElement).selectedOptions[0].value).then(datos=>{
-        (selectPaises as HTMLSelectElement).innerHTML=""
-        var optionInicial:HTMLOptionElement= document.createElement("option");
-       (optionInicial as HTMLOptionElement).value="0";
-       (optionInicial as HTMLOptionElement).text="Seleccciona pais...";
-       datos.forEach((pais: { name: { common: string; }; }) => {
-        let option=document.createElement("option");
-        (option as HTMLOptionElement).value=pais.name.common;
-        (option as HTMLOptionElement).text=pais.name.common;
-        selectPaises?.appendChild(option)
-       });
-        
 
-     })
+
+document.querySelector("#continente")?.addEventListener("change", () => {
+    let h1 = document.querySelector("#error");
+    (h1 as HTMLHeadingElement).innerHTML = ""
+    let selectContinentes = document.querySelector("#continente")
+    let selectPaises = document.querySelector("#paises")
+    let value = (selectContinentes as HTMLSelectElement).selectedOptions[0].value;
+    if (value === "0") {
+        (selectPaises as HTMLSelectElement).innerHTML = "";
+        let option = document.createElement("option");
+        option.value = "0";
+        option.text = "Seleciona Pais....";
+        (h1 as HTMLHeadingElement).innerHTML = "Deber seleccionar un continente"
+        selectPaises?.appendChild(option)
+    }
+
+    paises.getDatos("https://restcountries.com/v3.1/region/" +
+        (selectContinentes as HTMLSelectElement).selectedOptions[0].value).then(datos => {
+            (selectPaises as HTMLSelectElement).innerHTML = ""
+            var optionInicial = document.createElement("option");
+            optionInicial.value = "0";
+            optionInicial.text = "Seleccciona pais...";
+            selectPaises?.appendChild(optionInicial)
+            datos.forEach((pais:
+                {
+                    name: {
+                        common: string;
+                        official:string;
+                    };
+                    translations: {
+                        spa: {
+                            official: string;
+                        };
+                        
+                    };
+                }) => {
+                // (pais) me pongo encima del error y sale correccion rápida
+                let option = document.createElement("option");
+                option.value = pais.name.common;
+                if (pais.translations?.spa === undefined)
+                    option.text = pais.name?.official;
+                else
+                    option.text = pais.translations?.spa?.official;
+                selectPaises?.appendChild(option)
+            });
+
+
+        })
 })
