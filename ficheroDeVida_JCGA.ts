46553f85b6d86634dc204574f73bc154978af263
Juan Carlos <jcgarcia@dreamroomworld.com>
2023-11-15 12:01:10 +0100

    Planteamiento ejercicio

--- ficheroDeVida_JCGA.ts
+++ ficheroDeVida_JCGA.ts
@@ -22,32 +22,61 @@ paises.getDatos("https://restcountries.com/v3.1/region/europe").then(datos => {
 
 // Segundo ejercicio, para tener un selector de continente, en función de el, para que le pase los nombre de los paises en ese continente yield, en función del elegido, que empiece a escribir parte de sus datos por pantalla 
 
-
-document.querySelector("#continente")?.addEventListener("change",()=>{
-    // Lo vinculo con un listener para cuando cambie en el selector de continente
-   let selectContinentes=document.querySelector("#continente")
-   let selectPaises=document.querySelector("#paises")
-    // Doy de alta las 2 variables vinculadas a los id
-    
-    // A continuación le paso a la web, lo que quiero que me devuelva..., es decir, los paises de la region seleccionada
-    // Para ello, le tengo que decir que selctContinentes tiene que interpretarlo como un objeto de tipo SelectElement, en lugar de solo Element(que es su padre), que es lo que haría si no le hicieramos la conversión
-    // Y le tengo que decri que es un array de cero para que no espere más que un valor
-    paises.getDatos("https://restcountries.com/v3.1/region/"+
-     (selectContinentes as HTMLSelectElement).selectedOptions[0].value).then(datos=>{
-        (selectPaises as HTMLSelectElement).innerHTML=""
-        // Lo anterior, para que me lo deje en blanco, el selector de paises
-        // Y a continuación, dado que lo hemos dejado en blanco, le meto la opción por defecto... Y ESTO; DE MOMENTO NO FUNCIONA
-        var optionInicial:HTMLOptionElement= document.createElement("option");
-       (optionInicial as HTMLOptionElement).value="0";
-       (optionInicial as HTMLOptionElement).text="Seleccciona pais...";
-        // Para cada pais encontrado en el continente, le creo una entrada en el selector de paises
-       datos.forEach((pais: { name: { common: string; }; }) => {
-        let option=document.createElement("option");
-        (option as HTMLOptionElement).value=pais.name.common;
-        (option as HTMLOptionElement).text=pais.name.common;
+// Lo vinculo con un listener para cuando cambie en el selector de continente
+document.querySelector("#continente")?.addEventListener("change", () => {
+    let h1 = document.querySelector("#error");
+    (h1 as HTMLHeadingElement).innerHTML = ""
+    let selectContinentes = document.querySelector("#continente")
+    let selectPaises = document.querySelector("#paises")
+     // Doy de alta las 2 variables vinculadas a los id
+    let value = (selectContinentes as HTMLSelectElement).selectedOptions[0].value;
+    // Meto un if para tratar si no ha elegido nada
+    if (value === "0") {
+        (selectPaises as HTMLSelectElement).innerHTML = "";
+        let option = document.createElement("option");
+        option.value = "0";
+        option.text = "Seleciona Pais....";
+        (h1 as HTMLHeadingElement).innerHTML = "Deber seleccionar un continente"
         selectPaises?.appendChild(option)
-       });
-        
+    }
+//     // A continuación le paso a la web, lo que quiero que me devuelva..., es decir, los paises de la region seleccionada
+//     // Para ello, le tengo que decir que selctContinentes tiene que interpretarlo como un objeto de tipo SelectElement, en lugar de solo Element(que es su padre), que es lo que haría si no le hicieramos la conversión
+//     // Y le tengo que decri que es un array de cero para que no espere más que un valor
+    paises.getDatos("https://restcountries.com/v3.1/region/" +
+        (selectContinentes as HTMLSelectElement).selectedOptions[0].value).then(datos => {
+            // Lo anterior, para que me lo deje en blanco, el selector de paises
+            // Y a continuación, dado que lo hemos dejado en blanco, le meto la opción por defecto
+            (selectPaises as HTMLSelectElement).innerHTML = ""
+            var optionInicial = document.createElement("option");
+            optionInicial.value = "0";
+            optionInicial.text = "Seleccciona pais...";
+            selectPaises?.appendChild(optionInicial)
+            // Para cada pais encontrado en el continente, le creo una entrada en el selector de paises
+            datos.forEach((pais:
+                {
+                    name: {
+                        common: string;
+                        official:string;
+                    };
+                    translations: {
+                        spa: {
+                            official: string;
+                            common: string;
+                        };
+                        
+                    };
+                }) => {
+                // Si solo pongo (pais), me da error luego en los common, porque me dice que no tiene definido esa variable. me pongo encima del error y sale corrección rápida
+                let option = document.createElement("option");
+                option.value = pais.name.common;
+                if (pais.translations?.spa === undefined)
+                    option.text = pais.name?.common;
+                else
+                    option.text = pais.translations?.spa?.common;
+                // El "?" dice que aunque falta algo por indefinido, siga la ejecución. Si no, si no encuentra un pais traducido, se pararía
+                    selectPaises?.appendChild(option)
+            });
 
-     })
+
+        })
 })
