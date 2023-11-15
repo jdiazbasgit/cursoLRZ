a0189ae6245c917e0369946f6bfc1fb7c9bb6ac1
lujandroid <lujandroid@gmail.com>
2023-11-15 12:54:32 +0100

    cambio

--- dist/ficheroDeVida_jpt.js
+++ dist/ficheroDeVida_jpt.js
@@ -17,18 +17,38 @@ let div = document.querySelector("#contenido");
     div?.appendChild(h1)
 })*/
 (_a = document.querySelector("#continente")) === null || _a === void 0 ? void 0 : _a.addEventListener("change", () => {
+    let h1 = document.querySelector("#error");
+    h1.innerHTML = "";
     let selectContinentes = document.querySelector("#continente");
     let selectPaises = document.querySelector("#paises");
+    let value = selectContinentes.selectedOptions[0].value;
+    if (value === "0") {
+        selectPaises.innerHTML = "";
+        let option = document.createElement("option");
+        option.value = "0";
+        option.text = "Seleciona Pais....";
+        h1.innerHTML = "Deber seleccionar un continente";
+        selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
+    }
     paises.getDatos("https://restcountries.com/v3.1/region/" +
-        selectContinentes.selectedOptions[0].value).then(datos => {
+        selectContinentes.selectedOptions[0].value).then((datos) => {
         selectPaises.innerHTML = "";
         var optionInicial = document.createElement("option");
         optionInicial.value = "0";
         optionInicial.text = "Seleccciona pais...";
+        selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(optionInicial);
+        datos.sort((a, b) => {
+            return a.translations.spa.common.localeCompare(b.translations.spa.common);
+        });
         datos.forEach((pais) => {
+            var _a, _b, _c, _d;
+            // (pais) me pongo encima del error y sale corrección rápida
             let option = document.createElement("option");
             option.value = pais.name.common;
-            option.text = pais.name.common;
+            if (((_a = pais.translations) === null || _a === void 0 ? void 0 : _a.spa) === undefined)
+                option.text = (_b = pais.name) === null || _b === void 0 ? void 0 : _b.common;
+            else
+                option.text = (_d = (_c = pais.translations) === null || _c === void 0 ? void 0 : _c.spa) === null || _d === void 0 ? void 0 : _d.common;
             selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
         });
     });
