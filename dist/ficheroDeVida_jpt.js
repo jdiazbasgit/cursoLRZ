bc5156daa7630297fad5c54a8cedb105d824e443
lujandroid <lujandroid@gmail.com>
2023-11-15 12:05:05 +0100

    cambio

---
+++ dist/ficheroDeVida_jpt.js
@@ -0,0 +1,35 @@
+var _a;
+import { Paises } from "./clases/Paises_jpt.js";
+let paises = new Paises();
+let paisesRecibidos = [];
+let div = document.querySelector("#contenido");
+/*paises.getDatos("https://restcountries.com/v3.1/region/europe").then(datos => {
+    paisesRecibidos = datos
+  
+    paisesRecibidos.forEach(pais => {
+        let h1 = document.createElement("h1")
+        h1.innerHTML = `${pais.name.official} - ${pais.capital}`
+        div?.appendChild(h1)
+    })
+}).catch(error=>{//respuesta del error
+    let h1= document.createElement("h1")
+    h1.innerHTML=error
+    div?.appendChild(h1)
+})*/
+(_a = document.querySelector("#continente")) === null || _a === void 0 ? void 0 : _a.addEventListener("change", () => {
+    let selectContinentes = document.querySelector("#continente");
+    let selectPaises = document.querySelector("#paises");
+    paises.getDatos("https://restcountries.com/v3.1/region/" +
+        selectContinentes.selectedOptions[0].value).then(datos => {
+        selectPaises.innerHTML = "";
+        var optionInicial = document.createElement("option");
+        optionInicial.value = "0";
+        optionInicial.text = "Seleccciona pais...";
+        datos.forEach((pais) => {
+            let option = document.createElement("option");
+            option.value = pais.name.common;
+            option.text = pais.name.common;
+            selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
+        });
+    });
+});
