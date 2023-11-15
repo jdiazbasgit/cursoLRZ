93347004710de8be275d3379a0838146c44ae6af
dayanadae <147381066+dayanadae@users.noreply.github.com>
2023-11-15 12:16:28 +0100

    archivos ejercicio

---
+++ profe/dist/ficheroDeVida.js
@@ -0,0 +1,52 @@
+var _a;
+import { Paises } from "./clases/Paises.js";
+let paises = new Paises();
+let paisesRecibidos = [];
+let div = document.querySelector("#contenido");
+/*paises.getDatos("https://restcountries.com/v3.1/region/europe").then(datos => {
+    paisesRecibidos = datos
+  
+    paisesRecibidos.forEach(pais => {
+        let h1 = document.createElement("h1")
+        h1.innerHTML = `${pais.name.official} - ${pais.capital}`
+        div?.appendChild(h1)
+    })
+}).catch(error=>{//respuesta del error
+    let h1= document.createElement("h1")
+    h1.innerHTML=error
+    div?.appendChild(h1)
+})*/
+(_a = document.querySelector("#continente")) === null || _a === void 0 ? void 0 : _a.addEventListener("change", () => {
+    let h1 = document.querySelector("#error");
+    h1.innerHTML = "";
+    let selectContinentes = document.querySelector("#continente");
+    let selectPaises = document.querySelector("#paises");
+    let value = selectContinentes.selectedOptions[0].value;
+    if (value === "0") {
+        selectPaises.innerHTML = "";
+        let option = document.createElement("option");
+        option.value = "0";
+        option.text = "Seleciona Pais....";
+        h1.innerHTML = "Deber seleccionar un continente";
+        selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
+    }
+    paises.getDatos("https://restcountries.com/v3.1/region/" +
+        selectContinentes.selectedOptions[0].value).then(datos => {
+        selectPaises.innerHTML = "";
+        var optionInicial = document.createElement("option");
+        optionInicial.value = "0";
+        optionInicial.text = "Seleccciona pais...";
+        selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(optionInicial);
+        datos.forEach((pais) => {
+            var _a, _b, _c, _d;
+            // (pais) me pongo encima del error y sale corrección rápida
+            let option = document.createElement("option");
+            option.value = pais.name.common;
+            if (((_a = pais.translations) === null || _a === void 0 ? void 0 : _a.spa) === undefined)
+                option.text = (_b = pais.name) === null || _b === void 0 ? void 0 : _b.common;
+            else
+                option.text = (_d = (_c = pais.translations) === null || _c === void 0 ? void 0 : _c.spa) === null || _d === void 0 ? void 0 : _d.common;
+            selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
+        });
+    });
+});
