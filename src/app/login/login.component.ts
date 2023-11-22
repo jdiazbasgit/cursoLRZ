b1d626a55ea90d4ad97c99856d0b2cbd0adcdfd7
Your Name <you@example.com>
2023-11-22 11:56:39 +0100

    up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ErrorComponent} from '../error/error.component'
+import { ErrorComponent } from '../error/error.component'
 
 @Component({
   selector: 'app-login',
@@ -7,11 +7,24 @@ import {ErrorComponent} from '../error/error.component'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-texto:string="pepe"
-  pulsarBoton=()=>
-  {
-  let dato:ErrorComponent= new ErrorComponent()
-    alert(dato.texto)
+  texto: string = "pepeluis"
+  ver: boolean = false
+  numeros: Array<number> = [4, 6, 7, 89, 5, 2]
+  letras:Array<string>=["dee","ffdd","rtet","dee","ffdd","rtet","dee","ffdd","rtet"];
+  cambiarValor = () => {
+    this.texto = "He cambiado el texto"
+
   }
 
+  verTexto = () => {
+    alert(this.texto)
+  }
+
+  cambiarVer = () => {
+    this.ver = !this.ver
+  }    
+
+  ordenarNumeros=()=>{
+    this.numeros.sort()
+  }
 }
