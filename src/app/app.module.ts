09d1f38dcc87f04c3c98d44ef62489f0027e2325
dayanadae <147381066+dayanadae@users.noreply.github.com>
2023-11-23 10:05:05 +0100

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { FormsModule } from '@angular/forms';
 import { PruebaComponent } from './prueba/prueba.component';
+import { EjemploTirarComponent } from './ejemplo-tirar/ejemplo-tirar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ErrorComponent,
-    PruebaComponent
+    PruebaComponent,
+    EjemploTirarComponent
   ],
   imports: [
     BrowserModule,
