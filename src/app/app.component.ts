043dd8bfd570206a5b971f5d80cf2be5e4c799d6
marsanchispa <marsanchispa@hotmail.com>
2023-11-28 10:12:46 +0100

    copia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto de paises';
+  title = 'Proyecto de paises con parámetros dinámicos';
 }
