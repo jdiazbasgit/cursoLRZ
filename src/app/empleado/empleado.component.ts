0a23482f88603e00e849b8cced3a9c17e6345c02
Your Name <you@example.com>
2023-11-30 13:06:31 +0100

    cambios

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent {
-
+  mensajeDelDirector: string =""
+  mensajeAlDirector: string =""
 }
