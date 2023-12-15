5dcad808bbaecf270c3384e9dfe2684834997403
marsanchispa <marsanchispa@hotmail.com>
2023-12-15 13:46:01 +0100

    resuelto volcado array formularios

--- src/app/empresas/empresas.component.ts
+++ src/app/empresas/empresas.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
+
+interface Empresa {
+  puesto: string;
+  nombre: string;
+  enlace: string;
+}
 
 @Component({
   selector: 'app-empresas',
@@ -6,14 +13,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./empresas.component.css']
 })
 export class EmpresasComponent {
-  puestos= new Array();
-  empresas= new Array();
-  enlaces= new Array();
+  empresas: Empresa[] = [];
+  empresaData={puesto:"", nombre:"", enlace: "",}
 
-  guardaDatos(puesto:string,empresa:string,enlace:string){
-    this.puestos.push(puesto)
-    this.empresas.push(empresa)
-    this.enlaces.push(enlace)
+  guardaDatos(empresaData:Empresa) {
+    let nuevaEmpresa: Empresa = this.empresaData;
+    this.empresas.push(nuevaEmpresa);
+    this.empresaData = { puesto: '', nombre: '', enlace: '' };
   }
-
 }
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-empresas',
+//   templateUrl: './empresas.component.html',
+//   styleUrls: ['./empresas.component.css']
+// })
+// export class EmpresasComponent {
+//   puestos= new Array();
+//   empresas= new Array();
+//   enlaces= new Array();
+
+//   guardaDatos(puesto:string,empresa:string,enlace:string){
+//     this.puestos.push(puesto)
+//     this.empresas.push(empresa)
+//     this.enlaces.push(enlace)
+//   }
+
+// }
