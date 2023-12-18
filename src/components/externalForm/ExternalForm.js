6756a2f9fef71068c2f5860ce05c3243ca54b667
Javier <javier7moreno@hotmail.com>
2023-12-18 10:52:14 +0100

    type number en los campos nuevos del formulario

--- src/components/externalForm/ExternalForm.js
+++ src/components/externalForm/ExternalForm.js
@@ -9,38 +9,38 @@ const ExternalForm = () => {
         <div>
           <div style={{ marginLeft: "-330px" }}>
             <label>Dieta pernocta nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "18px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "18px" }}></input>
           </div>
           <div style={{ marginLeft: "-330px", marginTop: "13px" }}>
             <label>Dieta desayuno nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "13px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "13px" }}></input>
           </div>
           <div style={{ marginLeft: "-330px", marginTop: "13px" }}>
             <label>Dieta comida nacional</label>
-            <input type="number" style={{ width: "60px", height: "25px", marginLeft: "30px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "30px" }}></input>
           </div>
           <div style={{ marginLeft: "-330px", marginTop: "13px" }}>
             <label>Dieta cena nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "48px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "48px" }}></input>
           </div>
         </div>
         {/* Dietas Internacionales */}
         <div style={{ marginTop: "-138px" }}>
           <div style={{ marginLeft: "320px" }}>
             <label>Dieta pernocta nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "18px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "18px" }}></input>
           </div>
           <div style={{ marginLeft: "320px", marginTop: "13px" }}>
             <label>Dieta desayuno nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "13px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "13px" }}></input>
           </div>
           <div style={{ marginLeft: "320px", marginTop: "13px" }}>
             <label>Dieta comida nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "30px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "30px" }}></input>
           </div>
           <div style={{ marginLeft: "320px", marginTop: "13px" }}>
             <label>Dieta cena nacional</label>
-            <input style={{ width: "60px", height: "25px", marginLeft: "48px" }}></input>
+            <input type='number' style={{ width: "60px", height: "25px", marginLeft: "48px" }}></input>
           </div>
         </div>
     </div>
