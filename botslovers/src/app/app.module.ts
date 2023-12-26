426c2d1a901c47a148e5c4f57875e18026a9870c
AnaMisera <ana.misera@gmail.com>
2023-12-26 23:30:34 +0100

    Actualizadocompprincipal

--- botslovers/src/app/app.module.ts
+++ botslovers/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
