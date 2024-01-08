8c1f98b325202de8f7f406f4d07621b308309dbc
ManuelNoriega <mnoriegacosta@gmail.com>
2024-01-08 18:20:37 +0100

    actualizo componente modelodenegocio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ModelodenegocioComponent } from './modelodenegocio/modelodenegocio.comp
 
 const routes: Routes = [
   {path: "", component:   HomeComponent},
+  {path: "home", component:   HomeComponent},
   {path: "quienessomos", component:   QuienessomosComponent},
   {path:"modelodenegocio", component: ModelodenegocioComponent}
 ];
