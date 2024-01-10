2af07046c2935b497cb0177ac8803238e2ca2a16
gandialorente <gandialorente@gmail.com>
2024-01-10 10:48:47 +0100

    Cambios 100124

--- botslovers/src/app/app-routing/app-routing.module.ts
+++ botslovers/src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
-import { OpinionesComponent } from '../opiniones/opiniones.component';
+import { QuienessomosComponent } from '../quienessomos/quienessomos.component';
 
-const routes: Routes = [{ path: 'app-home', component: HomeComponent },{ path: 'app-opiniones', component: OpinionesComponent } ];
+const routes: Routes = [{ path: '', component: HomeComponent },{ path: 'app-quienessomosComponent', component: QuienessomosComponent } ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- botslovers/src/app/app.component.ts
+++ botslovers/src/app/app.component.ts
@@ -7,13 +7,15 @@ import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
 import { FeaturesComponent } from './features/features.component';
 import { OpinionesComponent } from "./opiniones/opiniones.component";
+import { QuienessomosComponent } from "./quienessomos/quienessomos.component";
+import { ModelodenegocioComponent } from "./modelodenegocio/modelodenegocio.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, FeaturesComponent, OpinionesComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, FeaturesComponent, OpinionesComponent, QuienessomosComponent, ModelodenegocioComponent]
 })
 export class AppComponent {
   title = 'botslovers';
